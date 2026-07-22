2f87fdfb957e41cf0031f17f4bf7e9cb47384f27
Davi Barbosa Placco França da Silva <davibplacco@gmail.com>
2026-07-22 23:44:34 +0100

    fix: remove github pages config for coolify deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/davi-finance",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
